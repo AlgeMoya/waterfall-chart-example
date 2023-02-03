65403813e5ea20cf5f306a7bf22411a25dd17e54
AlgeMoya <kjhobd2@naver.com>
2023-02-04 02:03:20 +0900

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+// https://github.com/alxnddr/waterfall-chart-example
+// https://javascript.plainenglish.io/how-to-create-a-waterfall-chart-in-react-with-visx-51ed1721dc77
+// https://airbnb.io/visx/gallery
+
 import { ParentSize } from "@visx/responsive";
 import { WaterfallChart } from "./WaterfallChart";
 import "./App.css";
+import React from "react";
+import BarGroupExample from "./BarGroupExample";
 
-const data = [
+const data2 = [
   { month: "Jan", earnings: 23 },
   { month: "Feb", earnings: 18 },
   { month: "Mar", earnings: -14 },
@@ -17,6 +23,13 @@ const data = [
   { month: "Dec", earnings: 5 },
 ];
 
+export type BarGroupProps = {
+  width: number;
+  height: number;
+  margin?: { top: number; right: number; bottom: number; left: number };
+  events?: boolean;
+};
+
 function App() {
   return (
     <div className="App">
@@ -25,13 +38,14 @@ function App() {
           <WaterfallChart
             width={width}
             height={height}
-            data={data}
+            data={data2}
             xAccessor={(datum) => datum.month}
             yAccessor={(datum) => datum.earnings}
             yLabel="Earnings"
           />
         )}
       </ParentSize>
+      <BarGroupExample width={1000} height={1000} />
     </div>
   );
 }
